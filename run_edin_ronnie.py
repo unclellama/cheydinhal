35fef4036d2d3b0ec0ae98831857dae800d23294
unclellama <unclellama@gmail.com>
2026-01-13 15:24:20 +0100

    added some 'helpful' code comments

--- run_edin_ronnie.py
+++ run_edin_ronnie.py
@@ -10,7 +10,7 @@ from thoravej29_dicts import tv29_colors, tv29_blocklist
 ml1 = load_single_ical(file1)
 ml2 = load_single_ical(file2)
 meetings = combine_meetings([ml1, ml2])
-categorize_meetings(meetings)
+
 meetings = filter_meetings(meetings, dates = ['2025-01-01', '2025-12-31'])
 
 #for m in meetings:
@@ -19,12 +19,8 @@ meetings = filter_meetings(meetings, dates = ['2025-01-01', '2025-12-31'])
 people = find_people(meetings, colors = tv29_colors, blocklist = tv29_blocklist)
 
 # all connections
-g = make_graph(people, meetings)
+g = make_graph(people, meetings, filename = 'outputs/con_all.gexf')
 
 # remove connections that only occurred one time ('intro meetings')
 g2 = make_graph(people, meetings, filename = "outputs/con_trimmed.gexf", 
                     min_connections = 2)
-                    
-navne = list(g2.nodes)
-for navn in navne:
-    print(navn)
